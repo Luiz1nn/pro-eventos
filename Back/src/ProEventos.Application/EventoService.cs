fa6cfc6d4e25ed0898231c9aa45c1e839421a85d
Luis Fernando <luisfernandobr2003@gmail.com>
2022-01-19 17:22:20 -0300

    secao 6

---
+++ Back/src/ProEventos.Application/EventoService.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Threading.Tasks;
+using ProEventos.Domain;
+using ProEventos.Application.Contratos;
+using ProEventos.Persistence.Contratos;
+
+namespace ProEventos.Application
+{
+    public class EventoService : IEventoService
+    {
+        private readonly IGeralPersist _geralPersit;
+        private readonly IEventoPersist _eventoPersit;
+
+        public EventoService(IGeralPersist geralPersist, IEventoPersist eventoPersist)
+        {
+            _eventoPersit = eventoPersist;
+            _geralPersit = geralPersist;
+        }
+
+        public async Task<Evento> AddEventos(Evento model)
+        {
+            try
+            {
+                _geralPersit.Add<Evento>(model);
+                if (await _geralPersit.SaveChangesAsync())
+                {
+                    return await _eventoPersit.GetEventoByIdAsync(model.Id, false);
+                }
+
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public async Task<Evento> UpdateEvento(int eventoId, Evento model)
+        {
+            try
+            {
+                var evento = await _eventoPersit.GetEventoByIdAsync(eventoId, false);
+
+                if (evento == null) return null;
+
+                model.Id = evento.Id;
+                _geralPersit.Update(model);
+
+                if (await _geralPersit.SaveChangesAsync())
+                {
+                    return await _eventoPersit.GetEventoByIdAsync(model.Id, false);
+                }
+
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<bool> DeleteEvento(int eventoId)
+        {
+            try
+            {
+                var evento = await _eventoPersit.GetEventoByIdAsync(eventoId, false);
+
+                if (evento == null) throw new Exception("Evento para delete não encontrado");
+
+                _geralPersit.Delete<Evento>(evento);
+                return await _geralPersit.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<Evento[]> GetAllEventosAsync(bool includePalestrantes = false)
+        {
+            try
+            {
+                var eventos = await _eventoPersit.GetAllEventosAsync(includePalestrantes);
+
+                if (eventos == null) return null;
+
+                return eventos;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes = false)
+        {
+            try
+            {
+                var eventos = await _eventoPersit.GetAllEventosByTemaAsync(tema, includePalestrantes);
+
+                if (eventos == null) return null;
+
+                return eventos;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<Evento> GetEventoByIdAsync(int eventoId, bool includePalestrantes = false)
+        {
+            try
+            {
+                var eventos = await _eventoPersit.GetEventoByIdAsync(eventoId, includePalestrantes);
+
+                if (eventos == null) return null;
+
+                return eventos;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
